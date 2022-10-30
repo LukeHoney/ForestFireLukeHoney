91158727201f94bfc156d36602eeac042910d0fa
LukeHoney <2248102@swansea.ac.uk>
2022-10-30 14:54:33 +0000

    Uploading Chicken Activation Script

--- Assets/ChickenActivation.cs
+++ Assets/ChickenActivation.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class ChickenActivation : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other) 
+    public FollowingScript followingScript; //refencing following script
+   
+    private void OnTriggerEnter(Collider other) //function what in range of the Chicken
     {
-        Debug.Log("Collition detected"); //logging if detection occuered 
+        if (other.tag == "Bread") //what happens when the Bread is in range of the Chicken
+        {
+            Debug.Log("The Chicken can smell the bread"); //logging if the chicken is in range of the bread
+            followingScript.enabled = true; //enabling foll0w scrip for when the chicken is close to the bread it will now follow. 
+        }
+
     }
+    
 }
--- Assets/Scripts/FollowingScript.cs
+++ Assets/Scripts/FollowingScript.cs
@@ -6,11 +6,15 @@ public class FollowingScript : MonoBehaviour
 {
     [SerializeField] private GameObject bread; //refecnce to bread for chicken to follow
     [SerializeField] private float speed = 1; //how fast the chicken moves
+    [SerializeField] private Transform breadDirection; //refernce to what chicken has to look at (Bread)
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position,bread.transform.position,speed * Time.deltaTime); // Telling the Chicken to follow the bread
+        transform.position = Vector3.MoveTowards(transform.position,breadDirection.transform.position,speed * Time.deltaTime); // Telling the Chicken to follow the bread
         Debug.Log("Chicken is now following the Bread"); //Shows in log when Chicken is following the bread
+
+        transform.LookAt(breadDirection); //code to make chicken look at the target (Bread)
+        Debug.Log("Chicken is now looking at the Bread"); //Shows in log when Chicken is looking at the bread
     }
 
 }
